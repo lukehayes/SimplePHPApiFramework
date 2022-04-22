ef3cbee0ca564ab02ad9a452e1f47915ed501185
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-22 09:34:51 +0100

    Added RouterService to project.

---
+++ src/Service/RouterService.php
@@ -0,0 +1,22 @@
+<?php
+namespace LH\Api\Service;
+
+use LH\Api\Service\Service;
+use LH\Api\Router;
+
+class RouterService extends Service
+{
+    public ?Router $router = NULL;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
+
+    public function get() : Object
+    {
+        dump($this->router);
+        return $this->router;
+    }
+}
+
