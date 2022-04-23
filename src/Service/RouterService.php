547615f6792612861671533f94b9a411f9912d39
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-23 15:49:35 +0100

    RouterService constructor now returns instance of Router.

--- src/Service/RouterService.php
+++ src/Service/RouterService.php
@@ -6,13 +6,21 @@ use LH\Api\Router;
 
 class RouterService extends Service
 {
+    /**
+     * @var $router */
     public ?Router $router = NULL;
 
     public function __construct()
     {
         $this->router = new Router();
+        return $this->router;
     }
 
+    /**
+     * Get the underlying service.
+     *
+     * @return Object
+     */
     public function get() : Object
     {
         return $this->router;
