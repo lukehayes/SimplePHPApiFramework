cad9b7d78f83a9ea45882329f031121824f52a07
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-25 10:40:51 +0100

    Added Container member anf getter to App class.

--- src/App.php
+++ src/App.php
@@ -2,11 +2,25 @@
 
 namespace LH\Api;
 
+use LH\Api\Container;
 
 class App
 {
+    private $container = NULL;
+
     public function __construct()
     {
+        $this->container = new Container();
+    }
+
+    /**
+     * Service container getter.
+     *
+     * @return LH\Api\Container
+     */
+    public function getContainer() : Container
+    {
+        return $this->container;
     }
 }
 
