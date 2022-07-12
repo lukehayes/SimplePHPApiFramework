4aee1575c014f7b3cd7d6da8f438b24f2fd519cd
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-12 09:02:41 +0100

    Added Auth\Auth class to project.

---
+++ src/Auth/Auth.php
@@ -0,0 +1,13 @@
+<?php
+namespace LH\Api\Auth;
+
+/**
+ * Class represents a very basic authentication system
+ */
+class Auth
+{
+	public function __construct()
+	{
+		dump("Auth class created");
+	}
+}
